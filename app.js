d5a302aef6365dcd54bfa817125849053e506147
anehellyperrony <153025974+Anehelly@users.noreply.github.com>
2024-06-20 09:26:04 -0300

    att projeto

--- app.js
+++ app.js
@@ -9,17 +9,42 @@ function adicionar(){
     let categoria = document.getElementById('icategoria');
     let classe = categoria.value
 
-  
-    if(classe === 'frutas'){
-        frutas.push(item);        
-    }
+    let adicionado = document.querySelector('#adicionado')
+    adicionado.innerHTML = ('Novo produto adicionado!')
+    
+    lista.push(item);   
+    
+    limparCampo()    
+    console.log(lista)  
 
-   
+}
+
+
+
+function limparCampo(){
+    let produto = document.getElementById('iitem');
+    produto.value = '';
+    let categoria = document.getElementById('icategoria');
+    categoria.value = '';
+    let listaCompleta = document.querySelector('p')
+    listaCompleta.innerHTML= '';
+    
+}
+
+function criarNova(){
+    
+        lista = []; // Limpa a lista de compras
+        document.getElementById('adicionado').textContent = '';
+        document.getElementById('listaCompleta').textContent = 'Lista de compras: Adicione novos itens à sua lista';
+    
+    
 }
 
 function mostrarLista(){
+    limparCampo()
     let listaCompleta = document.querySelector('p')
-    listaCompleta.innerHTML= (`Lista de compras: ${frutas.join(', ')}`)
+    listaCompleta.innerHTML= (`Lista de compras: ${lista.join(', ')}`)
+    let adicionado = document.querySelector('#adicionado')
+    adicionado.innerHTML = '';
 }
-
 
